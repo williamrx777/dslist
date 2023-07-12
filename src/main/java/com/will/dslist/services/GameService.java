f8dd2feac703eb9a87b4f8657415d2eeee1a49bc
will <williamr723@gmail.com>
2023-07-12 00:03:00 -0300

    Get GameById

--- src/main/java/com/will/dslist/services/GameService.java
+++ src/main/java/com/will/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.will.dslist.services;
 
+import com.will.dslist.dto.GameDTO;
 import com.will.dslist.dto.GameMinDTO;
 import com.will.dslist.repositories.GameRepository;
 import com.will.dslist.entities.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,6 +17,12 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(@PathVariable Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         var result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
