2edd595480aea48cd7a81b7c8fddadb5d97b7637
will <williamr723@gmail.com>
2023-07-13 23:49:09 -0300

    Correção de homologação

--- src/main/java/com/will/dslist/dto/GameMinDTO.java
+++ src/main/java/com/will/dslist/dto/GameMinDTO.java
@@ -28,9 +28,10 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
-        year = projection.getYear();
+        year = projection.getGameYear();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
+
     }
 
     public long getId() {
@@ -41,7 +42,7 @@ public class GameMinDTO {
         return title;
     }
 
-    public Integer getYear() {
+    public Integer getGameYear() {
         return year;
     }
 
